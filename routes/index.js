60538d151546b689304a9a403408a4eaa229655f
kudefe <kudefeg@gmail.com>
2017-09-28 17:48:45 -0300

    penultimo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+var request = require('superagent');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dev Say' });
 });
 
+
+
 module.exports = router;
+
+
