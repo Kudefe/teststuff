ccbb406dc16e273563d4b6003d6fe4cab71a178a
kudefe <kudefeg@gmail.com>
2017-10-08 21:12:12 -0300

    flows

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,12 @@
+$('body').flowtype();
+
+$('body').flowtype({
+    minimum   : 250,
+    maximum   : 750,
+    minFont   : 12,
+    maxFont   : 40,
+    fontRatio : 30
+ });
+
+
 (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);
